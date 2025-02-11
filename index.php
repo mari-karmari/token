b4a7d25bdfc1bf479dc69ed9f6065d64042a0470
gaborgaborgabor <kovacs@h-sechs.de>
2025-02-11 16:54:17 +0100

    Day1 Stuff from the simpe SSR in PHP
    here is my code from today for you to reventilate

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=<device-width>, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="css.css">
+</head>
+<body>
+    <h1>Hello</h1>
+
+   
+    
+
+
+
+
+
+
+<nav class="main_navi">
+
+<?php
+
+if ($_GET['page'] == 1) {
+    echo "
+    <a class='active' href='index.php?page=1'>Home</a>
+    <a class='' href='index.php?page=2'>About me</a>
+    <a class='' href='index.php?page=3'>Shop</a>
+    ";
+    require_once 'page1.php';
+}
+
+if ($_GET['page'] == 2) {
+    echo "
+    <a class='' href='index.php?page=1'>Home</a>
+    <a class='active' href='index.php?page=2'>About me</a>
+    <a class='' href='index.php?page=3'>Shop</a>
+    ";
+    require_once 'page2.php';}
+
+
+if ($_GET['page'] == 3) {
+    echo "
+    <a class='' href='index.php?page=1'>Home</a>
+    <a class='' href='index.php?page=2'>About me</a>
+    <a class='active' href='index.php?page=3'>Shop</a>
+    ";
+    require_once 'page3.php';}
+
+
+
+
+
+
+?>
+</nav>
+
+
+
+
+
+</body>
+</html>
